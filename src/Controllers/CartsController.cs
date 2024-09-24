5fb6a875fd4c4c52c3eb128c23b0007d5cbd5fe4
fedamousa <feeda2030@gmail.com>
2024-09-24 13:47:00 +0300

    add cart controller

--- src/Controllers/CartsController.cs
+++ src/Controllers/CartsController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace bookStore
+namespace BookStore
 {
     [ApiController]
     [Route("api/v1/[controller]")]
