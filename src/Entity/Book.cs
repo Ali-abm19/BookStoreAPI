c89f546f0d2d8140b2bfaa542dc23c606200fde6
Ali-abm19 <hugeali643@gmail.com>
2024-10-04 01:29:32 +0300

    Book annotations and fixed put & delete endpoints

--- src/Entity/Book.cs
+++ src/Entity/Book.cs
@@ -1,16 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BookStore.src.Entity;
 
 public class Book
 {
+    [Key]
     public Guid BookId { get; set; }
-    public string Isbn { get; set; }
+    public string? Isbn { get; set; }
+    [Required]
     public string Title { get; set; }
-    public string Author { get; set; }
+    public string? Author { get; set; }
+    [Range(1, double.MaxValue, ErrorMessage = "Price is not valid")]
     public double Price { get; set; }
+    [Range(1, double.MaxValue, ErrorMessage = "Stock Quantitiy is not valid")]
     public int StockQuantity { get; set; }
     public Format BookFormat { get; set; }
 
     //connections to other entities
+    //[ForeignKey("CategoryCategoryId")]
     public required Guid CategoryId { get; set; }
     public Category Category { get; set; }
 }
